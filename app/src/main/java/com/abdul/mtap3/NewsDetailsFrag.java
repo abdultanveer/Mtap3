476a011699533f544e4e0827b643ead9c4bd6129
abdul <abdul.tanveer@gmail.com>
2021-08-04 12:07:17 +0530

    fragments - intro

---
+++ app/src/main/java/com/abdul/mtap3/NewsDetailsFrag.java
@@ -0,0 +1,35 @@
+package com.abdul.mtap3;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.NotNull;
+
+public class NewsDetailsFrag extends Fragment {
+
+    @Override
+    public void onAttach(@NonNull @NotNull Context context) {
+        super.onAttach(context);
+    }
+
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.frag_news_details,container,false);
+        return  view;
+    }
+}
